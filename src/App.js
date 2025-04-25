814dffd36d6b4a7fce42e1d3af32d2a364d8f9aa
HarshitaDas2209 <130504068+HarshitaDas2209@users.noreply.github.com>
2025-04-25 23:40:13 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import DoctorDashboard from './components/DoctorDashboard';
+
+
+function App() {
+  return (
+    <div>
+      <DoctorDashboard/>
+    </div>
+  );
+}
+
+export default App;
